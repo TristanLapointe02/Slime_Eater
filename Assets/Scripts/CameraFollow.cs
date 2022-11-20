5a85aef7c3e4c8073729c3b7bdce83e5470cee3b
SamSeg10 <samseguin32@hotmail.ca>
2022-11-20 18:39:19 -0500

    Caméra change distance selon taille joueur

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,12 +7,14 @@ public class CameraFollow : MonoBehaviour
     public Transform target; //Target de la camera
     public float smoothSpeed; //Vitesse a laquelle la camera smooth
     public Vector3 offset; //Offset de la caméra
+    public Vector3 distanceJoueur; // Distance entre caméra et joueur
 
     private void FixedUpdate()
     {
         //Changer la position de la camera
         //Trouver la position
-        Vector3 positionCam = target.position + offset;
+        distanceJoueur.z = offset.z - (target.localScale.magnitude * 5); // Changer distance entre caméra et joueur selon sa taille
+        Vector3 positionCam = target.position + offset + distanceJoueur;
 
         //Smooth avec un lerp
         Vector3 smoothPositionCam = Vector3.Lerp(transform.position, positionCam, smoothSpeed);
